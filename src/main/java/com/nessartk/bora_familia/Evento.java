053a4d935537be5a3ce87d242364f79231808cbb
Vanessa Rutkoski <nessartk@gmail.com>
2025-04-18 15:06:46 -0300

    enum para TipoEvento, ajustes classe Evento

--- src/main/java/com/nessartk/bora_familia/Evento.java
+++ src/main/java/com/nessartk/bora_familia/Evento.java
@@ -3,11 +3,25 @@ package com.nessartk.bora_familia;
 import java.time.LocalDateTime;
 
 public class Evento {
+    private Long id;
     private String titulo;
     private String descricao;
-    private LocalDateTime horarioInicio;
-    private LocalDateTime horarioTermino;
+    private LocalDateTime dataHoraInicio;
+    private LocalDateTime dataHoraTermino;
     private String localizacao;
+    private TipoEvento tipo;
+    private Usuario responsavel;
+    private boolean notificar;
+    private boolean concluido;
+    private String cor;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getTitulo() {
         return titulo;
@@ -25,20 +39,20 @@ public class Evento {
         this.descricao = descricao;
     }
 
-    public LocalDateTime getHorarioInicio() {
-        return horarioInicio;
+    public LocalDateTime getDataHorarioInicio() {
+        return dataHoraInicio;
     }
 
-    public void setHorarioInicio(LocalDateTime horarioInicio) {
-        this.horarioInicio = horarioInicio;
+    public void setDataHorarioInicio(LocalDateTime dataHorarioInicio) {
+        this.dataHoraInicio = dataHorarioInicio;
     }
 
-    public LocalDateTime getHorarioTermino() {
-        return horarioTermino;
+    public LocalDateTime getDataHorarioTermino() {
+        return dataHoraTermino;
     }
 
-    public void setHorarioTermino(LocalDateTime horarioTermino) {
-        this.horarioTermino = horarioTermino;
+    public void setDataHorarioTermino(LocalDateTime dataHorarioTermino) {
+        this.dataHoraTermino = dataHorarioTermino;
     }
 
     public String getLocalizacao() {
@@ -47,5 +61,45 @@ public class Evento {
 
     public void setLocalizacao(String localizacao) {
         this.localizacao = localizacao;
+    }
+
+    public TipoEvento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEvento tipo) {
+        this.tipo = tipo;
+    }
+
+    public Usuario getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(Usuario responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public boolean isNotificar() {
+        return notificar;
+    }
+
+    public void setNotificar(boolean notificar) {
+        this.notificar = notificar;
+    }
+
+    public boolean isConcluido() {
+        return concluido;
+    }
+
+    public void setConcluido(boolean concluido) {
+        this.concluido = concluido;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
     }
 }
---
+++ src/main/java/com/nessartk/bora_familia/TipoEvento.java
@@ -0,0 +1,13 @@
+package com.nessartk.bora_familia;
+
+public enum TipoEvento {
+    ANIVERSARIO,
+    REUNIAO_ESCOLA,
+    TAREFA,
+    CONSULTA,
+    ATIVIDADE_ESCOLAR,
+    ATIVIDADE,
+    COMUNICADO_ESCOLAR,
+    OUTRO,
+
+}
