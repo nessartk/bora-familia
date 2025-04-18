2f20f5cf165fe88bb254288734b976abf5e42faa
Vanessa Rutkoski <nessartk@gmail.com>
2025-04-18 12:19:41 -0300

    first commit

---
+++ src/main/java/com/nessartk/bora_familia/BoraFamiliaApplication.java
@@ -0,0 +1,13 @@
+package com.nessartk.bora_familia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoraFamiliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoraFamiliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nessartk/bora_familia/BoraFamiliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nessartk.bora_familia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoraFamiliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
