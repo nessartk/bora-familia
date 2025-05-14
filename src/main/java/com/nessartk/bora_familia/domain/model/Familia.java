f683a959c133183a1f21cf15822c431669f4f48e
Vanessa Rutkoski <nessartk@gmail.com>
2025-05-14 17:05:35 -0300

    metodos classe familia e membro

--- src/main/java/com/nessartk/bora_familia/domain/model/Familia.java
+++ src/main/java/com/nessartk/bora_familia/domain/model/Familia.java
@@ -1,6 +1,8 @@
 package com.nessartk.bora_familia.domain.model;
 
 import jakarta.persistence.*;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,16 +14,15 @@ public class Familia {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    private String nome;
+    private String nomeFamilia;
 
     private LocalDateTime dataCriacao;
 
-    @OneToMany(mappedBy = "familia", cascade = CascadeType.ALL)
-    private List<Membro> membros = new ArrayList<>();
 
-    public Familia(UUID id, String nome){
+
+    public Familia(UUID id, String nomeFamilia){
         this.id = id;
-        this.nome = nome;
+        this.nomeFamilia = nomeFamilia;
     }
 
     public UUID getId() {
@@ -32,12 +33,12 @@ public class Familia {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
+    public String getNomeFamilia() {
+        return nomeFamilia;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNome(String nomeFamilia) {
+        this.nomeFamilia = nomeFamilia;
     }
 
     public LocalDateTime getDataCriacao() {
@@ -48,30 +49,15 @@ public class Familia {
         this.dataCriacao = dataCriacao;
     }
 
-    public List<Membro> getMembros() {
-        return membros;
-    }
-
-    public void setMembros(List<Membro> membros) {
-        this.membros = membros;
+    public String informacoeFamilia(){
+        return "Família" +getNomeFamilia() + "Data de criação" + getDataCriacao() + "Identificador" + getId();
     }
 
 
-    public int getQuantidadeMembros(){
-        return membros.size();
-    }
 
-    public void adicionarMembro(){
-        membros.add((Membro) membros);// list
-    }
 
-    public void removerMembro(){
-        membros.remove((Membro) membros); // list
-    }
 
-    public String getInformacoesFamilia(){
-        return "Família"+ nome + "membros"+ membros.size() + "Data criação" + getDataCriacao();
-    }
+
 
 
 
